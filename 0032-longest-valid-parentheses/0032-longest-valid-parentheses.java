6c7cbe538bf1f88141eae419355ad5e6c9b27ee2
Sriram Mulukuntla <srirammulukuntla11@gmail.com>
2026-07-21 14:22:22 +0530

    Time: 5 ms (82.07%), Space: 46.7 MB (13.58%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+
+        Stack<Integer> stk = new Stack<>();
+        stk.push(-1);
+
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                stk.push(i);
+            } else {
+
+                stk.pop();
+
+                if (stk.isEmpty()) {
+                    stk.push(i);
+                } else {
+                    maxLen = Math.max(maxLen, i - stk.peek());
+                }
+            }
+        }
+
+        return maxLen;
+    }
+}
