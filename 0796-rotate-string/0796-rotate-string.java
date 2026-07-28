48f755893abf4a9c981b51131d473cffc37f82e0
Sriram Mulukuntla <srirammulukuntla11@gmail.com>
2026-07-28 20:29:38 +0530

    Time: 2 ms (44.65%), Space: 43.6 MB (21.44%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()) return false;
+        String allPoss=s+s;
+         return allPoss.contains(goal);
+         
+    }
+}
