e60db0e0b07286715b0364c2c1ccb076bf991926
Sriram Mulukuntla <srirammulukuntla11@gmail.com>
2026-07-29 21:41:25 +0530

    Time: 7 ms (79.35%), Space: 46.6 MB (71.99%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,9 +1,9 @@
 class Solution {
 
-    // Function to build LPS array (KMP preprocessing)
+    
     private void buildLPS(int[] lps, String s) {
-        int i = 0;   // length of previous longest prefix suffix
-        int j = 1;   // current index
+        int i = 0;  
+        int j = 1;   
 
         while (j < s.length()) {
             if (s.charAt(i) == s.charAt(j)) {
@@ -24,19 +24,19 @@ class Solution {
     public String shortestPalindrome(String s) {
         String rev = new StringBuilder(s).reverse().toString();
 
-        // Combine string
+        
         String combined = s + "#" + rev;
 
         int[] lps = new int[combined.length()];
         buildLPS(lps, combined);
 
-        // Length of longest palindromic prefix
+      
         int len = lps[combined.length() - 1];
 
-        // Remaining part
+      
         String remaining = s.substring(len);
 
-        // Reverse remaining and add in front
+        
         return new StringBuilder(remaining).reverse().toString() + s;
     }
 }
