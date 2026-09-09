8ea684a8a250b686f16dec58bc9830eb4f50df5e
Sriram Mulukuntla <srirammulukuntla11@gmail.com>
2026-09-09 15:37:35 +0530

    Time: 1 ms (99.84%), Space: 47.2 MB (73.32%) - LeetHub

---
+++ 3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static int findMaxValue(int[][] matrix,int col)
+    {
+        int max_value = Integer.MIN_VALUE;
+        for(int i = 0;i<matrix.length;i++)
+        {
+            max_value = Math.max(max_value,matrix[i][col]);
+        }
+        return max_value;
+    }
+    public int[][] modifiedMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] result = new int[n][m];
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j < m;j++)
+            {
+                if(matrix[i][j] == -1)
+                {
+                    int max_value = findMaxValue(matrix,j);
+                    result[i][j] = max_value;
+                }
+                else{
+                result[i][j] = matrix[i][j];
+                }
+            }
+        }
+        return result;
+    }
+}
