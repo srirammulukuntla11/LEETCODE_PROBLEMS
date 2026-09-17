1f74658edfb884503aa4344349e7b6eb2b362903
Sriram Mulukuntla <srirammulukuntla11@gmail.com>
2026-09-17 10:31:11 +0530

    Time: 33 ms (68.6%), Space: 43.6 MB (96.73%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,49 +1,23 @@
 class Solution {
-
-    static Boolean isSubsetSum(int arr[], int sum) {
+    public boolean canPartition(int[] arr) {
         int n = arr.length;
-        boolean dp[][] = new boolean[n][sum + 1];
-
-        // Base case: sum = 0 is always possible
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
+        int sum = 0;
+        for(int i = 0;i<n;i++)
+        {
+            sum+=arr[i];
         }
-
-        // Base case for first element
-        if (arr[0] <= sum) {
-            dp[0][arr[0]] = true;
-        }
-
-        // DP logic
-        for (int ind = 1; ind < n; ind++) {
-            for (int target = 1; target <= sum; target++) {
-
-                boolean notTake = dp[ind - 1][target];
-
-                boolean take = false;
-                if (target >= arr[ind]) {
-                    take = dp[ind - 1][target - arr[ind]];
-                }
-
-                dp[ind][target] = notTake || take;
+        if(sum%2 != 0) return false;
+        int target = sum/2;
+        boolean dp[] = new boolean[target+1];
+        dp[0] = true;
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = target;j>=arr[i];j--)
+            {
+                dp[j] = dp[j] || dp[j-arr[i]];
             }
+            
         }
-
-        return dp[n - 1][sum];
-    }
-
-    public boolean canPartition(int[] nums) {
-        int totSum = 0;
-
-        for (int i : nums) {
-            totSum += i;
-        }
-
-        // If total sum is odd → cannot partition
-        if (totSum % 2 != 0) return false;
-
-        int target = totSum / 2;
-
-        return isSubsetSum(nums, target);
+        return dp[target];
     }
 }
