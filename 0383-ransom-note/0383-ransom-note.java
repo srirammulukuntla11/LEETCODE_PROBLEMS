ef762c750508ff82ee7f2e451dfd7ac99d3139a0
Sriram Mulukuntla <srirammulukuntla11@gmail.com>
2026-09-23 13:21:53 +0530

    Time: 3 ms (50.8%), Space: 46.3 MB (79.38%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        StringBuilder sb = new StringBuilder(magazine);
+        for(char c : ransomNote.toCharArray())
+        {
+            int index = sb.indexOf(String.valueOf(c));
+            if(index == -1)
+            {
+                return false;
+            }
+            sb.deleteCharAt(index);
+
+        }
+        return true;
+
+
+
+    }
+}
