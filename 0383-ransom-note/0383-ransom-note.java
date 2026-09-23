92b1dc1b580be2fdab0eb86f505ca00207ceb4f9
Sriram Mulukuntla <srirammulukuntla11@gmail.com>
2026-09-23 13:41:59 +0530

    Time: 15 ms (32.27%), Space: 47.2 MB (19.98%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,17 +1,38 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        StringBuilder sb = new StringBuilder(magazine);
-        for(char c : ransomNote.toCharArray())
+        // StringBuilder sb = new StringBuilder(magazine);
+        // for(char c : ransomNote.toCharArray())
+        // {
+        //     int index = sb.indexOf(String.valueOf(c));
+        //     if(index == -1)
+        //     {
+        //         return false;
+        //     }
+        //     sb.deleteCharAt(index);
+
+        // }
+        // return true;
+        HashMap<Character,Integer> mpp = new HashMap<>();
+        for(char c : magazine.toCharArray())
         {
-            int index = sb.indexOf(String.valueOf(c));
-            if(index == -1)
+            mpp.put(c,mpp.getOrDefault(c,0)+1);
+        }
+        for(char ch : ransomNote.toCharArray())
+        {
+            if(!mpp.containsKey(ch) || mpp.get(ch) == 0)
             {
                 return false;
             }
-            sb.deleteCharAt(index);
-
+            
+            mpp.put(ch,mpp.get(ch)-1);
         }
         return true;
+
+
+
+
+
+
 
 
 
