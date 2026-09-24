fdc5538082864138985c4f4ea1d19774a7fd57b9
Sriram Mulukuntla <srirammulukuntla11@gmail.com>
2026-09-24 10:40:21 +0530

    Time: 0 ms (100%), Space: 43.4 MB (64.01%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,33 +2,48 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-
-        // 🚫 Start blocked
-        if(grid[0][0] == 1) return 0;
-
         int[][] dp = new int[m][n];
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-
-                // 🚫 Obstacle
-                if(grid[i][j] == 1) {
+        if(grid[0][0] == 1)
+        {
+            return 0;
+        }
+        dp[0][0] = 1;
+        for(int j = 1;j<n;j++)
+        {
+            if(grid[0][j] == 1)
+            {
+                dp[0][j] = 0;
+            }
+            else
+            {
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+        for(int i = 1;i<m;i++)
+        {
+            if(grid[i][0] == 1)
+            {
+                dp[i][0] = 0;
+            }
+            else
+            {
+                dp[i][0] = dp[i-1][0];
+            }
+        }
+        for(int i = 1;i<m;i++)
+        {
+            for(int j = 1;j<n;j++)
+            {
+                if(grid[i][j] == 1)
+                {
                     dp[i][j] = 0;
-                    continue;
                 }
-
-                // ✅ Start cell
-                if(i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    int up = (i > 0) ? dp[i - 1][j] : 0;
-                    int left = (j > 0) ? dp[i][j - 1] : 0;
-
-                    dp[i][j] = up + left;
+                else
+                {
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
                 }
             }
         }
-
-        return dp[m - 1][n - 1];
+        return dp[m-1][n-1];
     }
 }
